246c71c800f75c4b58d49bb3fa82aa8f0f3ac080
Alethepata <alessia.patane@gmail.com>
2024-06-18 16:59:22 +0200

    edit form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,40 +3,70 @@ import { useState } from 'react';
 import { BsTrash } from "react-icons/bs";
 
 function Form() {
+    const dataDefault = {
+        title: '',
+        description: ''
+    } 
+
     const [blogs, setBlogs] = useState([]);
-    const [titleBlog, setTitleBlog] = useState('');
+    const [blogData, setBlogData] = useState(dataDefault);
 
     const handleTitle = (event) => {
         event.preventDefault();
 
-        setBlogs([...blogs, titleBlog]);
+        setBlogs(array => ([...array, blogData]));
 
-        setTitleBlog('');
+        setBlogData(dataDefault);
     }
 
     const deleteTitle = indexTitle => { 
         setBlogs(blogs => blogs.filter((_, index) => indexTitle !== index))
     }
 
+    const addData = (key, newData) => {
+        setBlogData(data => ({...data, [key]: newData}))
+    }
+
     return (
         <div className="form">
             <h1>Form</h1>
             <form onSubmit={handleTitle}>
+
+                <div>
                     <label for="title">Titolo</label>
                     <input
                         id="title"
                         type="text"
-                        value={titleBlog}
-                        onChange={event => setTitleBlog(event.target.value)}
+                        value={blogData.title}
+                        onChange={event => addData('title', event.target.value)}
                     />
+                </div>
+
+                <div className="margin">
+                    <label for="description">Descrizione</label>
+                    <textarea
+                        id="description"
+                        value={blogData.description}
+                        onChange={event => addData('description', event.target.value)}
+                    ></textarea>
+                </div>
+
                 <button>Invia</button>
             </form>
             <div className="text-container">
                 {
-                    blogs.map((title, index) => (
-                        <div className="text">
-                            <h3 key={`title_${index}`}>{title}</h3>
-                            <button onClick={() => deleteTitle(index)}><BsTrash /></button>
+                    blogs.map((blog, index) => (
+                        <div key={`title_${index}`} className="text">
+
+                            <div className="title">
+                                <h3>{blog.title}</h3>
+                                <button onClick={() => deleteTitle(index)}><BsTrash /></button>
+                            </div>
+
+                            <div className="description">
+                                <p>{blog.description}</p>
+                            </div>
+
                         </div>
                     ))
                 }
